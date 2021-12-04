a31345196ac93e8669bcd309d88ffa27d021b402
sucoDeGoiaba <flp.pxxs@gmail.com>
2021-12-04 16:26:57 -0300

    refazendo o rolê todo

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
-// const cripto = document.
-// let escolhaCripto = cripto.addEventListener("click", function(){
-//     for(var i = 0; i< cripto.length; i++){
-//         if(cripto[i].checked){
-//             // retorna o valor do item marcado
-//             return cripto[i].value;
-//         }
-//     }
-//     console.log(escolhaCripto);
-// });
-
-// Cifra
-
-const listaDeCaracteresIgnorados = []
-
-var string = "textougaBuga";
-var stringConvertida;
-var numeroIncrementos = 5
-for(var i = 0; i < string.length; i++){
-    if(stringConvertida === undefined){
-        var iniciaEm = string[i].charCodeAt();
-        var terminaEm = iniciaEm + numeroIncrementos;
-        stringConvertida = String.fromCharCode(terminaEm)
-    } else if(){
-        continue;
+function criptografandoCifraDeCesar(texto, passos){
+    var textoCriptografado; 
+    for(var i = 0; i < texto.length; i++){
+        
+        if((texto[i].charCodeAt() >= 65 && texto[i].charCodeAt() <= 90) || (texto[i].charCodeAt() >= 97 && texto[i].charCodeAt() <= 122)){            
+            if(texto[i].charCodeAt() >= 97){
+                // Checa se é minuscula
+                if(textoCriptografado === undefined){
+                    textoCriptografado = String.fromCharCode((texto[i].charCodeAt() - 97 + passos) % 26 + 97);
+                } else {
+                    textoCriptografado += String.fromCharCode((texto[i].charCodeAt() - 97 + passos) % 26 + 97);
+                }
+            } else {
+                if(textoCriptografado === undefined){
+                    textoCriptografado = String.fromCharCode((texto[i].charCodeAt() - 65 + passos) % 26 + 65);
+                } else {
+                    textoCriptografado += String.fromCharCode((texto[i].charCodeAt() - 65 + passos) % 26 + 65);
+                }
+            }
+        } else {
+            if(textoCriptografado === undefined){
+                textoCriptografado = texto[i]
+            } else {
+                textoCriptografado += texto[i]
+            }
+        }
     }
-    var iniciaEm = string[i].charCodeAt();
-    var terminaEm = iniciaEm + numeroIncrementos;
-    stringConvertida += String.fromCharCode(terminaEm)
-};
+    return textoCriptografado;
+}
+console.log(criptografandoCifraDeCesar("Ugabuga/12Texto", 5));
 
+//Maiusc - vai De 65 até 90
 
-console.log(stringConvertida);
+//Minusc - vai de 97 até 122
