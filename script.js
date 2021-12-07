77b095d0cbaf35eb4eb77b5ec21bf289a1a6cba6
sucoDeGoiaba <flp.pxxs@gmail.com>
2021-12-07 08:55:07 -0300

    finalmente algum progresso no js

--- script.js
+++ script.js
@@ -30,24 +30,98 @@ function criptografandoCifraDeCesar(texto, passos){
 // console.log(criptografandoCifraDeCesar("Ugabuga/12Texto", 5));
 
 
-function pegandoTexto(){
-    var selecionandoTexto = document.querySelector("#texto");
-    return selecionandoTexto.innerText;
+// function pegandoTexto(){
+//     var selecionandoTexto = document.querySelector("#texto");
+//     return selecionandoTexto.innerText;
+// }
+
+// var escolhendoCifra = document.querySelector("#crifaDeCesar");
+// escolhendoCifra.addEventListener("click", function(){
+//     if(escolhendoCifra.checked === true){
+//         // Checar se há a "aba" de incrementos
+//         // Faltar arrumar isso
+//         escolhendoIncrementos = document.createElement("label");
+//         escolhendoIncrementos.id = "valorIncrementos";
+//         const containerEscolhaDeCripto = document.querySelector("#containerEscolhaDeCripto");
+//         containerEscolhaDeCripto.append(escolhendoIncrementos);
+        
+//         escolhendoIncrementos.innerHTML = `
+//         Digite o numero de incrementos:
+//         <input type="number" name="valorIncrementos" id="valorIncrementos">
+//         `;
+//     }
+// });
+
+// function criptografandoBase64(texto){
+//     // btoa() codfi
+//     // atob() decodfi
+// }
+
+
+// // var uga = {
+// //     pegandoTexto : function(){
+// //         var selecionandoTexto = document.querySelector("#texto");
+// //         return selecionandoTexto.innerText;
+// //     },
+// //     escolhendoCripto : function(){
+
+// //     }
+// // }
+
+let opcoesForm = {
+    criptoEscolhida : "",
+    setCriptoEscolhida : function(escolha){
+        this.criptoEscolhida = escolha;
+    },
+    acaoEscolhida : "",
+    setAcaoEscolhida : function(escolha){
+        this.acaoEscolhida = escolha;
+    }
 }
 
-var escolhendoCifra = document.querySelector("#crifaDeCesar");
+
+
+const escolhendoCifra = document.querySelector("#crifaDeCesar");
 escolhendoCifra.addEventListener("click", function(){
     if(escolhendoCifra.checked === true){
-        // Checar se há a "aba" de incrementos
-        // Faltar arrumar isso
-        escolhendoIncrementos = document.createElement("label");
-        escolhendoIncrementos.id = "incrementos";
-        const containerEscolhaDeCripto = document.querySelector("#containerEscolhaDeCripto");
-        containerEscolhaDeCripto.append(escolhendoIncrementos);
+
+        const containerIncrementos = document.querySelector(".containerIncrementos");
+        if(containerIncrementos.style.display === "none"){
+            containerIncrementos.style.display = "flex";
+        }
+
+        opcoesForm.setCriptoEscolhida("cifra");
+        // console.log(opcoesForm.criptoEscolhida);
     }
 });
 
-function criptografandoBase64(texto){
-    // btoa() codfi
-    // atob() decodfi
-}
+const escolhendoBase64 = document.querySelector("#base64");
+escolhendoBase64.addEventListener("click", function(){
+    // Faz sumir com a escolha de incrementos
+    const containerIncrementos = document.querySelector(".containerIncrementos");
+    if(containerIncrementos.style.display !== "none"){
+        containerIncrementos.style.display = "none";
+    }
+
+    if(escolhendoBase64.checked === true){
+        opcoesForm.setCriptoEscolhida("base64");
+        // console.log(opcoesForm.criptoEscolhida);
+    }
+});
+
+const acaoCriptografando = document.querySelector("#criptografar");
+acaoCriptografando.addEventListener("click", function(){
+    if(acaoCriptografando.checked === true){
+        opcoesForm.setAcaoEscolhida("criptografar");
+        // console.log(opcoesForm.acaoEscolhida);
+    }
+});
+
+const acaoDescriptografando = document.querySelector("#descriptografar");
+acaoDescriptografando.addEventListener("click", function(){
+    if(acaoDescriptografando.checked === true){
+        opcoesForm.setAcaoEscolhida("descriptografar");
+        // console.log(opcoesForm.acaoEscolhida);
+    }
+});
+
